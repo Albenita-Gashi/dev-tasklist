1e450fcf39f6d6c88e5efbdde847a0bb00cd9cad
Albenita Gashi <albenitagashi76@gmail.com>
2024-03-12 18:30:41 +0100

    Added form to post data to insert a booking

--- frontend/app/src/app/booking/create/page.tsx
+++ frontend/app/src/app/booking/create/page.tsx
@@ -1,42 +1,92 @@
 "use client"
-import { Input } from "antd";
+import { Button, DatePicker, DatePickerProps, Input, TimePicker } from "antd";
 import { UserOutlined, FieldTimeOutlined, CalendarOutlined, ProductOutlined } from '@ant-design/icons';
 import axios from "axios";
-import Link from "next/link";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import dayjs, { Dayjs } from "dayjs";
 
 
-type BookingType = {
+type BookingType1 = {
     id: number,
     doctor_name: string,
     end_time: string,
     service: string
-    date: string,
+    date: Date,
     start_time: string
 }
 
+export class BookingType {
+    constructor(
+        public doctor_name: string,
+        public service: string,
+        public date: Date,
+        public start_time: string,
+        public end_time: string
+        ) { }
+}
+
 const BookingList: React.FC = () => {
+    const [booking, setBooking] = useState<BookingType>(new BookingType('', '', '', '', ''));
+    const [start_time, setStart_time] = useState<Dayjs>();
+    const [end_time, setEnd_time] = useState<Dayjs>();
+    const [date, setDate] = useState<Dayjs>();
+    const [status, setStatus] = useState();
 
-    const [bookings, setBookings] = useState<BookingType[] | null>(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            axios.get("http://host.docker.internal:5000/api/bookings").then((res) => {
-                setBookings(res.data);
-            }).catch(e =>
-                console.error('Error fetching data:', e)
-            )
-        };
-        fetchData();
-    }, []);
+    const insertData = () => {
+        axios.post("http://host.docker.internal:5000/api/bookings", booking).then((res) => {
+            console.log(res);
+        }).catch(e =>
+            console.error('Error inserting data:', e)
+        )
+    }
+    
+    const onChangeDatePicker: DatePickerProps['onChange'] = (date, dateString) => {
+        setDate(date)
+        setBooking({ ...booking, date: date.format('YYYY-MM-DD')})
+    };
 
     return (
         <section>
-            <Input size="large" placeholder="service" prefix={<ProductOutlined />} />
-            <Input size="large" placeholder="doctor_name" prefix={<UserOutlined />} />
-            <Input size="large" placeholder="start_time" prefix={<FieldTimeOutlined />} />
-            <Input size="large" placeholder="end_time" prefix={<FieldTimeOutlined />} />
-            <Input size="large" placeholder="date" prefix={<CalendarOutlined />} />
+            <Input size="large"
+                placeholder="Service" value={booking?.service}
+                onChange={(e) => setBooking({ ...booking, service: e.target.value })}
+                prefix={<ProductOutlined />}
+            />
+            <Input size="large"
+                placeholder="Doctor Name" value={booking?.doctor_name}
+                onChange={(e) => setBooking({ ...booking, doctor_name: e.target.value })}
+                prefix={<UserOutlined />}
+            />
+            <TimePicker 
+                placeholder="Start Time"
+                size="large" 
+                format="h:mm a" 
+                onChange={(time: Dayjs, timeString: string) => {
+                    setBooking({ ...booking, start_time: timeString}),
+                    setStart_time(time)
+                }}
+                value = {start_time}
+            />
+            <TimePicker 
+                placeholder="End Time"
+                size="large" 
+                format="h:mm a"
+                onChange={(time: Dayjs, timeString: string) => {
+                    setBooking({ ...booking, end_time: timeString }),
+                    setEnd_time(time)
+                }}
+                value={end_time}
+            />
+            <DatePicker
+                size="large"
+                format={{
+                    format: 'YYYY-MM-DD',
+                    type: 'mask',
+                }}
+                value={date}
+                onChange={onChangeDatePicker}
+            />
+            <Button type="primary" onClick={() => insertData()}>Book Appointment</Button>
         </section>
     );
 };
