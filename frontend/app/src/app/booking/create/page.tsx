7ea5725fde2069d211f2bf6dc14545a8ea2d0e6b
Albenita Gashi <albenitagashi76@gmail.com>
2024-03-13 21:39:41 +0100

    Added style to homepage and book now page

--- frontend/app/src/app/booking/create/page.tsx
+++ frontend/app/src/app/booking/create/page.tsx
@@ -5,10 +5,11 @@ import axios from "axios";
 import { useEffect, useState } from 'react';
 import dayjs, { Dayjs } from "dayjs";
 import { BookingType } from "@/types/ModelTypes";
+import useDidMountEffect from "@/components/componentDidMountEffect";
 
 type NotificationType = 'success' | 'info' | 'warning' | 'error';
 
-const BookingList: React.FC = () => {
+const BookAppointment: React.FC = () => {
     const [booking, setBooking] = useState<BookingType>(new BookingType(1, '', '', '', '', ''));
     const [start_time, setStart_time] = useState<Dayjs>();
     const [end_time, setEnd_time] = useState<Dayjs>();
@@ -18,10 +19,12 @@ const BookingList: React.FC = () => {
     const [api, contextHolder] = notification.useNotification();
 
     useEffect(() => {
-        console.log("status");
-        
-        openNotification("success", "Booked Successfully!", statusMessage);
+        console.log(status);
     }, [status])
+
+    useDidMountEffect(() => {
+        openNotification("success", "Booked Successfully!", statusMessage);
+    }, [status]);
 
     const openNotification = (type: NotificationType, title: string, message: string) => {
         notification.open({
@@ -31,11 +34,8 @@ const BookingList: React.FC = () => {
     };
     const insertData = () => {
         axios.post("http://host.docker.internal:5000/api/bookings", booking).then((res) => {
-            console.log(res);
             setStatus(!status)
             setStatusMessage(res.data)
-            // openNotification1();
-            // openNotification("success", "Booked Successfully!", res.data);
         }).catch(e =>
             openNotification("error", "Error inserting data!", e)
         )
@@ -45,63 +45,58 @@ const BookingList: React.FC = () => {
         setDate(date)
         setBooking({ ...booking, date: date.format('YYYY-MM-DD') })
     };
-    const openNotification1 = () => {
-        notification.open({
-          message: 'Notification Title',
-          description:
-            'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-          onClick: () => {
-            console.log('Notification Clicked!');
-          },
-        });
-      };
     return (
-        <section>
-            <Button type="primary" onClick={openNotification1}>
-                Open the notification box
-            </Button>
-            <Input size="large"
-                placeholder="Service" value={booking?.service}
-                onChange={(e) => setBooking({ ...booking, service: e.target.value })}
-                prefix={<ProductOutlined />}
-            />
-            <Input size="large"
-                placeholder="Doctor Name" value={booking?.doctor_name}
-                onChange={(e) => setBooking({ ...booking, doctor_name: e.target.value })}
-                prefix={<UserOutlined />}
-            />
-            <TimePicker
-                placeholder="Start Time"
-                size="large"
-                format="h:mm a"
-                onChange={(time: Dayjs, timeString: string) => {
-                    setBooking({ ...booking, start_time: timeString }),
-                        setStart_time(time)
-                }}
-                value={start_time}
-            />
-            <TimePicker
-                placeholder="End Time"
-                size="large"
-                format="h:mm a"
-                onChange={(time: Dayjs, timeString: string) => {
-                    setBooking({ ...booking, end_time: timeString }),
-                        setEnd_time(time)
-                }}
-                value={end_time}
-            />
-            <DatePicker
-                size="large"
-                format={{
-                    format: 'YYYY-MM-DD',
-                    type: 'mask',
-                }}
-                value={date}
-                onChange={onChangeDatePicker}
-            />
-            <Button type="primary" onClick={() => insertData()}>Book Appointment</Button>
+        <section className="book-wrapper">
+            <div className="book">
+                <div className="book-backgroundImg"></div>
+                <p className="book-title">Book Appointment</p>
+                <div className="book-inputs">
+                    <Input size="large"
+                        placeholder="Service" value={booking?.service}
+                        onChange={(e) => setBooking({ ...booking, service: e.target.value })}
+                        prefix={<ProductOutlined />}
+                    />
+                    <Input size="large"
+                        placeholder="Doctor Name" value={booking?.doctor_name}
+                        onChange={(e) => setBooking({ ...booking, doctor_name: e.target.value })}
+                        prefix={<UserOutlined />}
+                    />
+                    <DatePicker
+                        size="large"
+                        format={{
+                            format: 'YYYY-MM-DD',
+                            type: 'mask',
+                        }}
+                        value={date}
+                        onChange={onChangeDatePicker}
+                    />
+                    <div>
+                        <TimePicker
+                            placeholder="Start Time"
+                            size="large"
+                            format="h:mm a"
+                            onChange={(time: Dayjs, timeString: string) => {
+                                setBooking({ ...booking, start_time: timeString }),
+                                    setStart_time(time)
+                            }}
+                            value={start_time}
+                        />
+                        <TimePicker
+                            placeholder="End Time"
+                            size="large"
+                            format="h:mm a"
+                            onChange={(time: Dayjs, timeString: string) => {
+                                setBooking({ ...booking, end_time: timeString }),
+                                    setEnd_time(time)
+                            }}
+                            value={end_time}
+                        />
+                    </div>
+                </div>
+                <Button type="primary" onClick={() => insertData()}>Book Appointment</Button>
+            </div>
         </section>
     );
 };
 
-export default BookingList;
+export default BookAppointment;
--- frontend/app/src/app/page.tsx
+++ frontend/app/src/app/page.tsx
@@ -1,20 +1,29 @@
-async function getBookings() {
-  const res = await fetch('http://host.docker.internal:5000/api/bookings', { cache: 'no-store', mode: 'no-cors' })
+// async function getBookings() {
+//   const res = await fetch('http://host.docker.internal:5000/api/bookings', { cache: 'no-store', mode: 'no-cors' })
+
+import { Button } from "antd";
+import Link from "next/link";
+import image from "@/assets/images/do.jpg"
+import Image from "next/image";
  
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
  
-  return res.json()
-}
+//   return res.json()
+// }
 
 const Home: React.FC = async () => {
 
-  const bookings = await getBookings()
+  // const bookings = await getBookings()
 
   return (
-    <div>
-      <h1>Current booking count: {bookings.length}</h1>
+    <div className="home">
+      <p className="home-title">Pabau Booking</p>
+      <div className="home-backgroundImg"></div>
+      <p className="home-subtitle">Book appointments with ease and convenience. Find the right doctor for your needs, schedule appointments hassle-free, and manage your medical visits effortlessly.</p>
+      <Link href="/booking/create"><button className="home-button">Book Now</button></Link>
+      {/* <h1>Current booking count: {bookings.length}</h1> */}
     </div>
   );
 };
